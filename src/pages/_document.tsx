84b7c070662f7c175220c8c76a4db19441be970b
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2023-04-03 07:25:11 +0700

    feat: add dark mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "@/config/theme";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
